c42f14fda9f261b7de0d89a6a8e412289b2a7e15
Patrick Mullins <pmullins11@mac.com>
2019-04-23 00:35:58 -0400

    Added Elixir source

---
+++ Source/vorlongGreeting.exs
@@ -0,0 +1,7 @@
+# Babylon 5 "..and so it begins" Code Project.
+# Description: The B5 equivalent of your traditional Hello World app.
+# Author: Patrick H. Mullins (patrick@arkmail.us) | Updated: 2018-12-12 @ 04:12AM
+# Programming Language: Elixir
+
+IO.puts "...and so it begins."
+
