abc08747cec8ec3f1844ed8bed62517f7ed76f14
Patrick Mullins <pmullins11@mac.com>
2019-04-26 02:00:05 -0400

    Renamed source files. Added Elixir example.

--- Source/b5code.exs
+++ Source/b5code.exs
@@ -1,3 +1,5 @@
+#!/usr/local/bin/elixir
+
 # Babylon 5 "..and so it begins" Code Project.
 # Description: The B5 equivalent of your traditional Hello World app.
 # Author: Patrick H. Mullins (patrick@arkmail.us) | Updated: 2018-12-12 @ 04:12AM
